1fa23004ff288809ead944d8546ac8911ddb96c0
kelgrin <kelgrin@mail.ru>
2019-11-11 13:33:33 +0600

    lab-4

---
+++ src/SimpleInheritance.java
@@ -0,0 +1,26 @@
+public class SimpleInheritance {
+    public static  void  call(String args[]){
+        A superOb = new A();
+        B subOb = new B();
+
+        //Суперкласс может использоваться самостоятельно
+        superOb.i=10;
+        subOb.j=20;
+        System.out.println("Содержимое объекта superOb:");
+        superOb.showij();
+        System.out.println();
+
+        //Подкласс имеет право доступа ко всем открытым членам своего суперкласса
+        subOb.i=7;
+        subOb.j=8;
+        subOb.k=9;
+        System.out.println("Содержимое объекта subOb:");
+        subOb.showij();
+        subOb.showk();
+
+        System.out.println();
+        System.out.println("Сумма i, j и k в объекте subOb: ");
+        subOb.sum();
+    }
+
+}
